b53ddf938a39e08c1b81b75e119aa197e2b13f32
Matt Cassara <cassaram09@gmail.com>
2018-09-27 19:16:28 +0000

    break lab

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  const arr = []
 
-  for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  for (let i = 0, i < musicians.length; i++) {
+    console.log(`${musicians[0]} plays ${instruments[0]}`)
   }
-
-  return arr
 }
 
 function johnLennonFacts(facts) {
-  const shoutedFacts = []
 
-  let i = 0
-  while (i < facts.length) {
-    shoutedFacts.push(`${facts[i]}!!!`)
-    i++
-  }
-
-  return shoutedFacts
 }
 
 function iLoveTheBeatles(n) {
-  const arr = []
 
-  do {
-    arr.push('I love the Beatles!')
-    n++
-  } while (n < 15)
-
-  return arr
 }
